22d662f4392d77f7ba6a94d21115dd12c1a3455e
SeetaRam <seetaramanjaneyulu172002@gmail.com>
2024-08-14 01:55:54 +0530

    A line graph for the cases fluctuations and map for showing cases for specific countries completed

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,22 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Toaster/>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Toaster />
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/ChartsAndMaps.tsx
+++ src/pages/ChartsAndMaps.tsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { CircleMarker, MapContainer, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  plugins,
+  scales,
+} from "chart.js";
+import { MoonLoader } from "react-spinners";
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
+
+const worldWideDataOfCases = async () => {
+  return await axios.get("https://disease.sh/v3/covid-19/all");
+};
+
+const countrySpecificCases = async () => {
+  return await axios.get("https://disease.sh/v3/covid-19/countries");
+};
+
+const graphDataForCases = async () => {
+  return await axios.get(
+    "https://disease.sh/v3/covid-19/historical/all?lastdays=all"
+  );
+};
 
 const ChartsAndMaps = () => {
-  return (
-    <div>ChartsAndMaps</div>
-  )
-}
 
-export default ChartsAndMaps
+  const { isLoading: isCountryLoading, data: countryData } = useQuery({
+    queryKey: ["country-specific-cases"],
+    queryFn: countrySpecificCases,
+  });
+
+  const { isLoading: isGraphDataLoading, data: graphData } = useQuery({
+    queryKey: ["graph-data-for-cases"],
+    queryFn: graphDataForCases,
+  });
+
+  const getTheCountryWiseData = () => {
+    const countryWideCases = countryData?.data.map((obj: any) => {
+      return {
+        country: obj.country,
+        active: obj.active,
+        recovered: obj.recovered,
+        deaths: obj.deaths,
+        lat: obj.countryInfo.lat,
+        lon: obj.countryInfo.long,
+      };
+    });
+
+    return (
+      <MapContainer
+        center={[20, 77]}
+        zoom={3}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer
+          noWrap={true}
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {countryWideCases.map((country: any, index: number) => (
+          <CircleMarker
+            key={index}
+            center={[country.lat, country.lon]}
+            radius={10}
+            color="blue"
+            fillColor="blue"
+            fillOpacity={0.5}
+          >
+            <Popup>
+              <div>
+                <strong>{country.country}</strong>
+                <br />
+                Active: {country.active}
+                <br />
+                Recovered: {country.recovered}
+                <br />
+                Deaths: {country.deaths}
+              </div>
+            </Popup>
+          </CircleMarker>
+        ))}
+      </MapContainer>
+    );
+  };
+
+  const getThePlottableData = () => {
+    const labels = Object.keys(graphData?.data.cases);
+    const data = Object.values(graphData?.data.cases);
+
+    const finalData = {
+      labels: labels,
+      datasets: [
+        {
+          labels: "Cases from past 3 years",
+          data: data,
+          backgroundColor: "aqua",
+          borderColor: "black",
+          pointBorderColor: "aqua",
+          fill: true,
+          tension: 0.4,
+        },
+      ],
+    };
+
+    return <Line data={finalData}></Line>;
+  };
+
+  return (
+    <div className="flex flex-col gap-y-10">
+      <div>
+        {isGraphDataLoading ? (
+          <div className="flex justify-center">
+            <MoonLoader size={60} color="#32e6be" speedMultiplier={0.5} />
+          </div>
+        ) : (
+          getThePlottableData()
+        )}
+      </div>
+      <div>
+        {isCountryLoading ? (
+          <div className="flex justify-center">
+            <MoonLoader size={60} color="#32e6be" speedMultiplier={0.5} />
+          </div>
+        ) : (
+          getTheCountryWiseData()
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChartsAndMaps;
